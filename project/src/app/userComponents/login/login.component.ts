8298bb4aa2ad274d302606a0faf35a7e581dc4e6
hagar hassan <hagarhassan813@gmail.com>
2022-10-20 01:34:47 +0200

    chick data from signup to login

--- project/src/app/userComponents/login/login.component.ts
+++ project/src/app/userComponents/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import{FormGroup, FormControl} from "@angular/forms";
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private _route:Router , private _http:HttpClient) { }
+  login:FormGroup|any;
   ngOnInit(): void {
+    this.login= new FormGroup({
+      "email": new FormControl(),
+      "password": new FormControl()
+    })
   }
 
-}
+  loginData(login:FormGroup){
+
+    console.log(this.login.value);//catch data from sign up
+    this._http.get<any>("http://localhost:3000/users").subscribe(
+      res=>{
+
+        const user = res.find((a:any)=>
+        {
+          return a.email === this.login.value.email && a.password === this.login.value.password;
+        });
+        if(user)
+        {
+          alert('you are successfully login');
+          this.login.reset();
+          this._route.navigate(['']);//rout home page ??????????????????????
+        }
+        else if(this.login.value.email == "admin@gmail.com" && this.login.value.password == "12345678")
+        {
+          alert('you are successfully login as Admin');
+          this._route.navigate(['Products']);
+          this.login.reset();
+        }
+        else{
+          alert('user not found');
+          this._route.navigate(['signup']);
+        }
+      }
+
+    )
+      }
+
+  }
+
--- project/src/app/userComponents/signup/signup.component.ts
+++ project/src/app/userComponents/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import{FormGroup, FormBuilder, FormControl} from "@angular/forms";
+import{FormGroup, FormControl} from "@angular/forms";
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 
