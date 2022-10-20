7ad7c4f643a00c158c990944e76ce635f6f7e8c7
workytip <workytip@gmail.com>
2022-10-20 03:37:22 +0200

    home -details - add to cart

--- project/src/app/app-routing.module.ts
+++ project/src/app/app-routing.module.ts
@@ -1,7 +1,52 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+<<<<<<< Updated upstream
 
 const routes: Routes = [];
+=======
+import { AddProductsComponent } from './Components/Admin/add-products/add-products.component';
+import { ErrorComponent } from './Components/Admin/error/error.component';
+import { ProductsComponent } from './Components/Admin/products/products.component';
+import { UpdateProductsComponent } from './Components/Admin/update-products/update-products.component';
+import { CartComponent } from './Components/User/cart/cart.component';
+import { HomeComponent } from './Components/User/home/home.component';
+import { ProductDetailsComponent } from './Components/User/product-details/product-details.component';
+
+const routes: Routes = [
+  {
+    path:"",
+    component:HomeComponent
+  },
+  {
+    path:"cart",
+    component:CartComponent
+  },
+  {
+    path:"product/:id",
+    component:ProductDetailsComponent
+  },
+  {
+    path:"admin",
+    component:ProductsComponent
+  },
+  {
+    path:"Products",
+    component:ProductsComponent
+  },
+  {
+    path:"AddProducts",
+    component:AddProductsComponent
+  },
+  {
+    path:"products/edit/:id",
+    component:UpdateProductsComponent
+  },
+  {
+    path:"**",
+    component:ErrorComponent
+  },
+];
+>>>>>>> Stashed changes
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -3,10 +3,45 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+<<<<<<< Updated upstream
 
 @NgModule({
   declarations: [
     AppComponent
+=======
+import { HeaderComponent } from './Components/Admin/header/header.component';
+import { ProductsComponent } from './Components/Admin/products/products.component';
+import { AddProductsComponent } from './Components/Admin/add-products/add-products.component';
+import { ErrorComponent } from './Components/Admin/error/error.component';
+import { ServicesService } from './Components/Admin/Services/services.service';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { UpdateProductsComponent } from './Components/Admin/update-products/update-products.component';
+import { DeleteProductsComponent } from './Components/Admin/delete-products/delete-products.component';
+import { HomeComponent } from './Components/User/home/home.component';
+import { CartComponent } from './Components/User/cart/cart.component';
+import { ProductDetailsComponent } from './Components/User/product-details/product-details.component';
+import { CheckoutComponent } from './Components/User/checkout/checkout.component';
+import { NavComponent } from './Components/User/nav/nav.component';
+import { ProductItemComponent } from './Components/User/product-item/product-item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ProductsComponent,
+    AddProductsComponent,
+    ErrorComponent,
+    UpdateProductsComponent,
+    DeleteProductsComponent,
+    HomeComponent,
+    CartComponent,
+    ProductDetailsComponent,
+    CheckoutComponent,
+    NavComponent,
+    ProductItemComponent,
+    
+>>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
