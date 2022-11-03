2460b22e8f844edc7779362fdf1a4a968332850c
Zzero202 <ahmedfaisaly2000@gmail.com>
2022-11-03 15:24:50 +0200

    edited apis

--- project/src/app/Components/Admin/add-products/add-products.component.ts
+++ project/src/app/Components/Admin/add-products/add-products.component.ts
@@ -44,6 +44,7 @@ uploadFile(event:Event){
 
 submitForm()
 {
+  
   const formData :any= new FormData();
      formData.append('image', this.form.controls['image'].value);
      formData.append('title', this.form.controls['title'].value);
@@ -52,31 +53,33 @@ submitForm()
      formData.append('price', this.form.controls['price'].value);
      console.log(formData);
      this.myService.AddProd(formData).subscribe();
-
-}
-
-
-
-
-  AddProductForm = new FormGroup({
-    title: new FormControl(""),
-    SKU: new FormControl("",[Validators.required,Validators.maxLength(8),Validators.minLength(0)]),
-    image:new FormControl(""),
-    price: new FormControl(0),
-    details: new FormControl("")
-  })
-  get SKUvalid(){
-    return this.AddProductForm.controls.SKU.valid
-  }
-  AddProd(){
-    if(this.AddProductForm.valid){
-      this.myService.AddProd(this.AddProductForm.value).subscribe();
-      alert("product is added")
+     alert("product is added")
       window.location.href = "/admin"    
-    }else{
-    // alert("error");
-    }
-
 
 }
+
+
+
+
+//   AddProductForm = new FormGroup({
+//     title: new FormControl(""),
+//     SKU: new FormControl("",[Validators.required,Validators.maxLength(8),Validators.minLength(0)]),
+//     image:new FormControl(""),
+//     price: new FormControl(0),
+//     details: new FormControl("")
+//   })
+//   get SKUvalid(){
+//     return this.AddProductForm.controls.SKU.valid
+//   }
+//   AddProd(){
+//     if(this.AddProductForm.valid){
+//       this.myService.AddProd(this.AddProductForm.value).subscribe();
+//       alert("product is added")
+//       window.location.href = "/admin"    
+//     }else{
+//     // alert("error");
+//     }
+
+
+// }
 }
