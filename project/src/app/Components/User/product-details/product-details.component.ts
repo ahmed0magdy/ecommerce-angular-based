5b40439232f3b9c06e422dc9817196ef3a4b1c4f
workytip <106924253+workytip@users.noreply.github.com>
2022-11-08 15:28:35 +0200

    add to cart from details

--- project/src/app/Components/User/product-details/product-details.component.ts
+++ project/src/app/Components/User/product-details/product-details.component.ts
@@ -11,6 +11,8 @@ export class ProductDetailsComponent implements OnInit {
 
   singleProduct:any;
   id:any;
+  productsInCart:any[]=[]
+
   imgsrc= 'http://localhost:8000/storage/images';
 
   quantity:number=1;
@@ -28,10 +30,46 @@ export class ProductDetailsComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
-  Addtocart()
+  plus()
   {
-    this.singleProduct.quanity=this.quantity;
-    this.item.emit(this.singleProduct);
+    this.quantity++;
   }
+  minus()
+  {
+    this.quantity--;
+    if(this.quantity <1)
+    {
+      this.quantity =1;
+    }
+  }
+
+  addtocart(){
+
+    if("cart" in localStorage)
+    {
+      this.productsInCart=JSON.parse(localStorage.getItem("cart")!); // updated array -- ! to escape null
+      let exist = this.productsInCart.find(item => item.id == this.singleProduct.id);
+      if(exist){
+        alert('Item Already chosen')
+      }
+      else
+      {
+        this.singleProduct.quanity=this.quantity;
+        this.productsInCart.push(this.singleProduct);
+        localStorage.setItem('cart',JSON.stringify(this.productsInCart) )
+
+      }
+
+    }
+    else
+    {
+      this.singleProduct.quanity=this.quantity;
+      this.productsInCart.push(this.singleProduct);
+      localStorage.setItem('cart',JSON.stringify(this.productsInCart) )
+    }
+
+    window.location.href='/'
+
+  }
+
 }
