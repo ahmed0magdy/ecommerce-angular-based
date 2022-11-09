ee062a243e99a321cca9c5da82a453f457feec3e
= <=>
2022-11-09 22:40:46 +0200

    user profile Done

--- project/src/app/Components/User/profile/profile.component.ts
+++ project/src/app/Components/User/profile/profile.component.ts
@@ -12,19 +12,32 @@ export class ProfileComponent implements OnInit {
   constructor(private myservice:ServicesService,activatedroute:ActivatedRoute) { }
   profile:any;
   id:any = 1;
+  orders:any[]=[];
   ngOnInit(): void {
     this.myservice.getByProfileId(this.id).subscribe((data)=>{
       this.profile = data;
-      console.log(data);
+     // console.log(data);
     })
 
   }
-  DeleteOrder(prod_id:any){
-    console.log(prod_id);
-    this.myservice.DeleteOrd(prod_id).subscribe((data)=>{
-      console.log(data);
+  DeleteOrder(order_id:any){
+    //console.log(order_id);
+    this.myservice.DeleteOrd(order_id).subscribe((data)=>{
+     // console.log(data);
       window.location.href = "/profiles";
     })
+  }
+  Orderchange(o_id:any){
+   // console.log(o_id);
+    for(let p of this.profile.productsorders){
+      if(o_id == p.order_id){
+    console.log(p);
+        this.orders.push(p);
+      }
+  }
+  }
+  orderdelete(){
+    this.orders = [];
   }
 
 }
