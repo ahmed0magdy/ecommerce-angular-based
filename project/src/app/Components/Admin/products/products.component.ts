ec50f1a22a60ae54f72e71533ea85b22311123cf
hagar hassan <hagarhassan813@gmail.com>
2022-11-10 14:29:03 +0200

    admin logic

--- project/src/app/Components/Admin/products/products.component.ts
+++ project/src/app/Components/Admin/products/products.component.ts
@@ -17,11 +17,12 @@ export class ProductsComponent implements OnInit {
   page:number = 1;
   total:number = 0;
   ngOnInit(): void {
-    this.LoggedInAdmin = localStorage.getItem("UserId")
-    if(this.LoggedInAdmin != 5){
-        window.location.href = '/';
+    // this.LoggedInAdmin = localStorage.getItem("UserId")
+    // if(this.LoggedInAdmin != 5){
+    //     window.location.href = '/';
     
-    }
+    // }
+    
     // if(localStorage.getItem("user")){
     //   // alert("welcome "+sessionStorage.getItem("userEmail"));
     // }
